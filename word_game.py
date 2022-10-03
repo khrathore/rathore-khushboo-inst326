96e9a49ab168feef09158b5d59b14c6a3ddd7074
khrathore <44534747+khrathore@users.noreply.github.com>
2022-10-03 13:47:24 -0400

    Changes to word_game

--- word_game.py
+++ word_game.py
@@ -16,23 +16,56 @@ import sys
 # main() function.
 
 class PlayerWords:
+    """A class that models a person's word choices in a game
+    
+    Attributes:
+        filepath (string): The location of the text file
+        words (set): The set of words from the file
+
+    """
     
     def __init__(self, filepath):
+        """Create an instance of the PlayerWords class using a given file
+
+        Args:
+            filepath (string): The location of the text file that contains the player's words
+            
+        Side Effects:
+            Creates a set of the words that were in the given file
+        """
         self.words = set()
         with open(filepath, 'r', encoding="utf-8") as file:
             for line in file:
-                line.strip()
+                line = line.strip()
                 self.words.add(line)
                 
     def score(self, PlayerWords):
+        """Count the points that the players earned
+
+        Args:
+            PlayerWords (PlayerWords): The object containing the list of words from the other player
+
+        Returns:
+            points (int): The combined score of the players
+        """
         points = 0
         combo = self.words.intersection(PlayerWords.words)
+        print(combo)
         for thing in combo:
             if len(thing) > 2:
                 points += (len(thing) - 2)
         return points
         
 def main(file1, file2):
+    """Takes in two files and finds that score
+
+    Args:
+        file1 (string): The file containing the first player's words
+        file2 (string): The file containing the second player's words
+        
+    Side Effects:
+        Prints the score of the team.
+    """
     p1 = PlayerWords(file1)
     p2 = PlayerWords(file2)
     score = p1.score(p2)
